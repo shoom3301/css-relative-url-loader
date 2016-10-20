5459200952774bfff1948eadaf8745e1c1c658a4
a.e.kazachenko <shoom3301@gmail.com>
2016-10-20 13:09:04 +0300

    css-relative-url-loader

---
+++ index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const loaderUtils = require('loader-utils');
+
+module.exports = function (content) {
+    const query = loaderUtils.parseQuery(this.query);
+
+    if (!query.root) {
+        throw new Error('shm-loader: root parameter is required!');
+    }
+    const relative = this.context.replace(query.root, '').substring(1);
+
+    return content.replace(/url\([\'\"]?([^'"]+)[\'\"]?\)/g,
+        (str, path) => 'url(\'' + path.join(relative, path) + '\')'
+    );
+};
